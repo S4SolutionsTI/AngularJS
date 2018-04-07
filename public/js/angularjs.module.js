447a9728a89ab5ff18ed42c3eb397eca7790f215
José Francisco da Silva IV <silvaivctd@gmail.com>
2018-04-07 20:47:41 -0300

    Refactoring code and fix bugs

--- public/js/angularjs.module.js
+++ public/js/angularjs.module.js
@@ -1,14 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* global angular */
 (function (angular) {
-    var app;
-    app = function () {
-        return angular
-                .module(
-                        'MyAngularApp',
-                        [],
-                        function ($httpProvider) {
-                            $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-                        });
-    };
+    'use strict';
+    angular
+            .module(
+                    'MyAngularApp',
+                    [],
+                    function ($httpProvider) {
+                        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+                    });
+
 }(angular));
--- public/js/controller/main.controller.js
+++ public/js/controller/main.controller.js
@@ -1,28 +1,33 @@
-(function() {
+/*global angular*/
+/*global console*/
+
+(function (angular, console) {
     'use strict';
 
-  angular
-    .module('MyAngularApp').controller('MainController', ($scope, $http) => {
-      $scope.testData = [];
-      $scope.postIt = function postIt() {};
-      (function postIt() {
-        // alert("posting...");
-        // XMLHttpRequest cannot load http://127.0.0.1:1337/. No
-        // 'Access-Control-Allow-Origin' header is present on the requested
-        // resource. Origin 'null' is therefore not allowed access.
-        $http.get('http://localhost:8081/json.json', {
-          msg: 'hello from Angular.js!',
-        }).success((response) => {
-          console.log(response);
-          // $scope.$apply(function(){
-          // $scope.testData = JSON.parse(response);
-          // console.log($scope.testData);
-          // });
-          $scope.testData = response;
-          // $scope.testData = JSON.parse(response);
-        }).error(() => {
-          console.log('AJAX failed!');
-        });
-      })
-    });
-}());
+    angular.module('MyAngularApp').controller('MainController',
+            function ($scope, $http) {
+                $scope.testData = [];
+                $scope.postIt = function postIt() {
+                };
+                (function postIt() {
+                    // alert("posting...");
+                    // XMLHttpRequest cannot load http://127.0.0.1:1337/. No
+                    // 'Access-Control-Allow-Origin' header is present on the
+                    // requested
+                    // resource. Origin 'null' is therefore not allowed access.
+                    $http.get('http://localhost:8081/json.json', {
+                        msg : 'hello from Angular.js!'
+                    }).success(function (response) {
+                        console.log(response);
+                        // $scope.$apply(function(){
+                        // $scope.testData = JSON.parse(response);
+                        // console.log($scope.testData);
+                        // });
+                        $scope.testData = response;
+                        // $scope.testData = JSON.parse(response);
+                    }).error(function () {
+                        console.log('AJAX failed!');
+                    });
+                })
+            });
+}(angular, console));
